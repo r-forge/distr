e2022128677574b5fc88952be0e556d3d8f64ec9
ruckdeschel (Prof. Dr ) <peter.ruckdeschel@uni-oldenburg.de>
2022-11-12 17:19:07 +0000

    updateTo_distr-2.9.0.R uploaded
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@1378 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/utils/updateTo_distr-2.9.0.R
@@ -0,0 +1,19 @@
+Values <- matrix(c("2.9.0", format(Sys.time(), format="%Y-%m-%d"),1370),nrow=3)
+colnames(Values) <- "distr"
+rownames(Values) <- c("Version","Date","VCS/SVNRevision")
+changeDescription(startDir = "C:/rtest/distr/",names=c("Version","Date","VCS/SVNRevision"),
+                  pkgs="distr", values=Values)
+
+Values <- matrix(c("1.2.3", format(Sys.time(), format="%Y-%m-%d"),1236),nrow=3,ncol=1)
+colnames(Values) <- "RobAstBase"
+rownames(Values) <- c("Version","Date","VCS/SVNRevision")
+changeDescription(startDir = "C:/rtest/robast/",names=c("Version","Date","VCS/SVNRevision"),
+                  pkgs="RobAstBase", values=Values, pathRepo="robast")
+
+Values <- matrix(c("1.3.0", format(Sys.time(), format="%Y-%m-%d"),1236),nrow=3,ncol=1)
+colnames(Values) <- "ROptEst"
+rownames(Values) <- c("Version","Date","VCS/SVNRevision")
+changeDescription(startDir = "C:/rtest/robast/",names=c("Version","Date","VCS/SVNRevision"),
+                  pkgs="ROptEst", values=Values, pathRepo="robast")
+
+
