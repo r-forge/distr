9c80f0c04e1a23669f46fccea06f3e76bd010b03
ruckdeschel (Prof. Dr ) <peter.ruckdeschel@uni-oldenburg.de>
2024-08-27 17:16:18 +0000

    [distr] (trunk) rebuilt tar ball for resubmission
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/distr@1448 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/distr/R/makedots.R
+++ pkg/distr/R/makedots.R
@@ -3,16 +3,22 @@
        dots$xlim <- dots$ylim <- dots$yaxt <- dots$axes <- dots$xaxt <- NULL
        dots$panel.last <- dots$panel.first <- dots$frame.plot <- dots$ann <-NULL
        dots$log <- dots$bg <- dots$asp <- NULL
+       # 20240822 added an additional layer of security
+       if (length(dots) == 0 ) dots <- NULL
        return(dots)
 }
 .makedotsL <- function(dots){
     dots <- .makedotsLowLevel(dots)
     dots$pch <- dots$cex <- NULL
+    # 20240822 added an additional layer of security
+    if (length(dots) == 0 ) dots <- NULL
     dots
 }
 .makedotsP <- function(dots){
     dots <- .makedotsLowLevel(dots)
     dots$lwd <- dots$lty <- NULL
+    # 20240822 added an additional layer of security
+    if (length(dots) == 0 ) dots <- NULL
     dots
 }
 .makedotsPt <- function(dots){
